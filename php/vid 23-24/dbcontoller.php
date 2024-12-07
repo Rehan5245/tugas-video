a56d13f05039544801c94ea9651b30e6af09f891
Rehan5245 <rehanheryansyah01@gmail.com>
2024-12-07 22:21:54 +0700

    suki

---
+++ php/vid 23-24/dbcontoller.php
@@ -0,0 +1,58 @@
+<?php
+
+    class DB {
+        
+        private $host = "127.0.0.1";
+        private $user = "root";
+        private $password = "";
+        private $database = "dbrestoran";
+        private $koneksi;
+
+        public function __construct() {
+            $this->koneksi = $this->koneksiDB();
+        }
+
+        public function koneksiDB() {
+            $koneksi = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+            return $koneksi;
+        }
+
+        public function getAll($sql) {
+            $result = mysqli_query($this->koneksi, $sql);
+            while ($row = mysqli_fetch_assoc($result)) {
+                $data[] = $row;
+            }
+            return $data;
+        }
+
+        public function getItem($sql) {
+            $result = mysqli_query($this->koneksi, $sql);
+            $row = mysqli_fetch_assoc($result);
+            return $row;
+        }
+
+        public function rowCount($sql) {
+            $result = mysqli_query($this->koneksi, $sql);
+            $count = mysqli_num_rows($result);
+            return $count;
+        }
+
+        public function runSQL($sql) {
+            $result = mysqli_query($this->koneksi, $sql);
+        }
+
+        public function console($text='') {
+            echo $text;
+        }
+
+    }
+
+    $db = new DB;
+
+    $db->console("Suki lo");
+
+    // var_dump($row);
+
+
+
+?>
